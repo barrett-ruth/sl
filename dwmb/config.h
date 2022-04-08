7e996eed2ed544fac1bd46529dbe286c25e83695
Barrett Ruth <br@barrett-ruth.com>
2022-04-07 21:01:46 -0500

    dwmblocks: use powerline finally

--- dwmb/config.h
+++ dwmb/config.h
@@ -1,12 +1,12 @@
 static const Block blocks[] = {
     /*Icon*/ /*Command*/ /*Update Interval*/ /*Update Signal*/
     {"", SCRIPT("bat.sh"), 5, 0},
-    {"", SCRIPT("vol.sh"), 0, 7},
-    {"", SCRIPT("luz.sh"), 0, 8},
+    {"", SCRIPT("vol.sh"), 0, 5},
+    {"", SCRIPT("luz.sh"), 0, 7},
     // {"", SCRIPT("cpu.sh"), 10, 0},
     // {"", SCRIPT("space.sh"), 10, 0},
-    {"", SCRIPT("wifi.sh"), 15, 8},
-    {"", SCRIPT("date.sh"), 60, 0}};
+    {"", SCRIPT("wifi.sh"), 5, 10},
+    {"", SCRIPT("date.sh"), 5, 0}};
 
 static char delim[] = "\0";
 static unsigned int delimLen = 1;
