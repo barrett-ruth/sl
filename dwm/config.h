aa9695c387a2c1ccc0c1ed1bc4a0418163a14842
Barrett Ruth <br@barrett-ruth.com>
2022-06-03 23:11:10 -0500

    remove xinerama; monospace font

--- dwm/config.h
+++ dwm/config.h
@@ -2,11 +2,11 @@
 #include <X11/XF86keysym.h>
 
 /* appearance */
-static const unsigned int borderpx = 7; /* border pixel of windows */
+static const unsigned int borderpx = 5; /* border pixel of windows */
 static const unsigned int snap = 32;    /* snap pixel */
 static const int showbar = 0;           /* 0 means no bar */
 static const int topbar = 1;            /* 0 means bottom bar */
-static const char *fonts[] = {"FiraCode Nerd Font Mono:size=14"};
+static const char *fonts[] = {"monospace:size=14"};
 static const char *colors[][3] = {
     /*               fg         bg         border   */
     [SchemeNorm] = {"#d4be98", "#3c3836", "#3c3836"},
@@ -51,20 +51,13 @@ static const Layout layouts[] = {
 static Key keys[] = {
     {MODKEY, XK_b, togglebar, {0}},
     {MODKEY, XK_c, spawn, SHCMD("chromium")},
-    {MODKEY, XK_w, spawn, SHCMD("qutebrowser")},
     {MODKEY, XK_space, spawn, SHCMD("dmenu_run")},
     {MODKEY, XK_Return, spawn, SHCMD("st")},
 
     /* Scripts */
     {SCRIPT, XK_d, spawn, SHCMD("doc")},
-    {SCRIPT, XK_e, spawn, SHCMD("edf")},
-    {SCRIPT, XK_h, spawn, SHCMD("pacmd set-default-sink 3")},
-    {SCRIPT, XK_m, spawn, SHCMD("mus")},
-    {SCRIPT, XK_p, spawn, SHCMD("pw")},
     {SCRIPT, XK_r, spawn, SHCMD("rbt")},
     {SCRIPT, XK_s, spawn, SHCMD("sdn")},
-    {SCRIPT, XK_t, spawn, SHCMD("xset m 0")},
-    {SCRIPT, XK_w, spawn, SHCMD("smon; sbg")},
 
     /* Window altering */
     {MODKEY, XK_a, focusstack, {.i = -1}},
@@ -92,10 +85,6 @@ static Key keys[] = {
     /* Fn keys */
     {0, XF86XK_MonBrightnessUp, spawn, SHCMD("luz up")},
     {0, XF86XK_MonBrightnessDown, spawn, SHCMD("luz down")},
-    {0, XF86XK_AudioRaiseVolume, spawn, SHCMD("vol up")},
-    {0, XF86XK_AudioLowerVolume, spawn, SHCMD("vol down")},
-    {0, XF86XK_AudioMute, spawn, SHCMD("vol mute")},
-    {0, XF86XK_AudioPlay, spawn, SHCMD("vol toggle")},
 
     TAGKEYS(XK_1, 0) TAGKEYS(XK_2, 1) TAGKEYS(XK_3, 2) TAGKEYS(XK_4, 3)
         TAGKEYS(XK_5, 4) TAGKEYS(XK_6, 5) TAGKEYS(XK_7, 6) TAGKEYS(XK_8, 7)
