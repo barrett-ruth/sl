d1f0d97d8d47f2563c54123e3522044b4dbf071d
Barrett Ruth <br@barrett-ruth.com>
2022-06-04 21:57:05 -0500

    dwmb: cleanups

--- dwmb/scripts/bat.sh
+++ dwmb/scripts/bat.sh
@@ -2,20 +2,6 @@
 
 bat="$(cat /sys/class/power_supply/BAT0/capacity)"
 
-format_chars() {
-    in="$1"
-
-    fill="$(printf "%$((in / 5))"s | sed 's| |-|g')"
-    rest="$(printf "%$((20 - in / 5))"s)"
-
-    echo "$fill$rest"
-}
-
-final_bar() {
-    spacing='   '
-    echo " $(format_chars "$bat")$spacing$bat"
-}
-
 [ "$(cat /sys/class/power_supply/BAT0/status)" = Charging ] && leave
 
 echo "^c#d4be98^$bat% "
