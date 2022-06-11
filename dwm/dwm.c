698e875d4cc11971cd186333a94cecf1b6df758d
Barrett Ruth <br@barrett-ruth.com>
2022-06-11 12:44:34 -0500

    dwm: remove synchronizing toggle bar, leaves a pixel uot

--- dwm/dwm.c
+++ dwm/dwm.c
@@ -1649,12 +1649,11 @@ void tile(Monitor *m) {
 }
 
 void togglebar(const Arg *arg) {
-  for (Monitor *m = mons; m; m = m->next) {
-    m->showbar = !m->showbar;
-    updatebarpos(m);
-    XMoveResizeWindow(dpy, m->barwin, m->wx, m->by, m->ww, bh);
-    arrange(m);
-  }
+  selmon->showbar = !selmon->showbar;
+  updatebarpos(selmon);
+  XMoveResizeWindow(dpy, selmon->barwin, selmon->wx, selmon->by, selmon->ww,
+                    bh);
+  arrange(selmon);
 }
 
 void toggleborders(const Arg *arg) {
