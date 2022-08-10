642e06940d38d219c8591730d449441d40741747
Barrett Ruth <br@barrettruth.com>
2022-08-10 17:00:34 -0500

    dwmb: clean up wifi script

--- dwmb/scripts/wifi.sh
+++ dwmb/scripts/wifi.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-ssid="$(iwctl station wlan0 get-networks | grep '>' | awk '{ print $2 }')"
-ssid="${ssid#????}"
+ssid="$(iwctl station wlan0 get-networks | grep '>' | awk '{ print $4 }')"
 ssid="${ssid:-n/a}"
 
 echo "^b#504945^ $ssid "
