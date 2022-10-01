d567b3e9ea815d70c1f5d990ae48c4d5af4b3be5
Barrett Ruth <br@barrettruth.com>
2022-09-30 22:06:03 -0400

    dmwb: vol

--- dwmb/config.h
+++ dwmb/config.h
@@ -1,7 +1,8 @@
 static const Block blocks[] = {
     /*Icon*/ /*Command*/ /*Update Interval*/ /*Update Signal*/
+    {"", SCRIPT("vol.sh"), 0, 5},
     {"", SCRIPT("bat.sh"), 5, 0},
-    {"", SCRIPT("wifi.sh"), 5, 7},
+    {"", SCRIPT("wifi.sh"), 5, 0},
     {"", SCRIPT("date.sh"), 5, 0}};
 
 static char delim[] = "\0";
---
+++ dwmb/scripts/vol.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+sink="$(pacmd list-sinks | grep '\* index: ' | tail -c 2)"
+
+vol=$(pactl get-sink-volume "$sink" | awk '{ print $5 }')
+vol="${vol%?}"
+[ "$(pactl get-sink-mute "$sink" | awk '{ print $2 }')" = 'yes' ] && muted=!
+
+vol=" $muted${vol:-n/a}"
+
+echo "$vol │ "
+
+unset vol muted sink
