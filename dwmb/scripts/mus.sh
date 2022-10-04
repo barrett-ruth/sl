ceed58ef35e1863e60268c17454127fc8d927176
Barrett Ruth <br@barrettruth.com>
2022-10-04 14:50:55 -0400

    dwmb: format music stirng

--- dwmb/scripts/mus.sh
+++ dwmb/scripts/mus.sh
@@ -2,8 +2,9 @@
 
 [ "$(ps aux | rg mpv)" ] || exit
 
-music="$(echo '{ "command": ["get_property", "filtered-metadata"] }' | socat - /tmp/mpvsocket | jq -r '[ .data.Artist, .data.Title ] | join (" - ")' | tr '\n' ' ')"
+music="$(echo '{ "command": ["get_property", "filtered-metadata"] } | join("\\:")' | socat - /tmp/mpvsocket | jq -r '[ .data.Artist, .data.Album, .data.Title ] | join(":")' | tr '\n' ' ' | sed 's/:/ - /;s/:/\//')"
 
+[ "${#music}" -gt 50 ] && music="$(echo "$music" | sed 's/-.*-/-/')"
 [ "$music" = '' ] || music=" $music│"
 
 echo "^b#504945^$music"
