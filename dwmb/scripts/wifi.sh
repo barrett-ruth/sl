578a8e3f85666a3e65a07e281b9a689a8d3e1cf8
Barrett Ruth <br.barrettruth@gmail.com>
2024-03-20 19:51:03 -0400

    dwmb: support wifi ethernet

--- dwmb/scripts/wifi.sh
+++ dwmb/scripts/wifi.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
 ssid="$(iwctl station wlan0 get-networks | grep '>' | awk '{ print $4 }')"
+if [ -z "$ssid" ]; then
+  if ping archlinux.org -c 1 >/dev/null 2>&1; then
+    ssid='eth'
+  fi
+fi
 ssid="${ssid:-n/a}"
 
 echo "$ssid │ "
