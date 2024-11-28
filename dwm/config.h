af95a4b6ea85a86ec42e7ad6c6cc94db7449a78a
Barrett Ruth <br.barrettruth@gmail.com>
2024-11-27 19:07:34 -0600

    fix chromium rule for proper WM_CLASS

--- dwm/config.h
+++ dwm/config.h
@@ -42,7 +42,7 @@ static const Rule rules[] = {
      *	WM_NAME(STRING) = title
      */
     /* class      instance    title       tags mask     isfloating   monitor */
-    {"chromium", NULL, NULL, 1 << 1, 0, -1},
+    {"Chromium", NULL, NULL, 1 << 1, 0, -1},
     {"sioyek", NULL, NULL, 1 << 2, 0, -1},
 };
 
