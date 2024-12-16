0f28f708dde584414f87b45d1ceba0321adf16cf
Barrett Ruth <br.barrettruth@gmail.com>
2024-12-16 14:12:56 -0600

    feat(dwmblocks): filler icon in notify-esnd

--- dwmblocks/scripts/bat.sh
+++ dwmblocks/scripts/bat.sh
@@ -1,22 +1,23 @@
 #!/bin/sh
 
 STATE_FILE="$XDG_STATE_HOME/".battery_notify_state
+filler_icon='/usr/share/icons/Adwaita/16x16/status/airplane-mode-symbolic.symbolic.png'
 
 bat="$(cat /sys/class/power_supply/BAT0/capacity)"
 status="$(cat /sys/class/power_supply/BAT0/status)"
 
 if [ "$status" = "Discharging" ]; then
-  if [ "$bat" -eq 24 ] && ! grep -q "^30$" "$STATE_FILE" 2>/dev/null; then
-    notify-send --urgency normal "Battery at 30%"
+  if [ "$bat" -eq 30 ] && ! grep -q "^30$" "$STATE_FILE" 2>/dev/null; then
+    notify-send --urgency normal -i "$filler_icon" "Battery at 30%"
     echo 30 >"$STATE_FILE"
   fi
 
   if [ "$bat" -eq 15 ] && ! grep -q "^15$" "$STATE_FILE" 2>/dev/null; then
-    notify-send --urgency critical "Battery at 15%"
+    notify-send --urgency critical -i "$filler_icon" "Battery at 15%"
     echo 15 >"$STATE_FILE"
   fi
 else
-  >"$STATE_FILE" 2>/dev/null
+  true>"$STATE_FILE" 2>/dev/null
 fi
 
 [ -n "$bat" ] && echo "$bat% | "
