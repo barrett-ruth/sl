cc20e282229ef7a828aa2d1de09c7f0f19ec1a9d
Barrett Ruth <br.barrettruth@gmail.com>
2025-01-21 19:39:38 -0500

    feat(dwmblocks): iwd script

--- dwmblocks/scripts/wifi.sh
+++ dwmblocks/scripts/wifi.sh
@@ -1,10 +1,30 @@
 #!/bin/sh
 
-station="$(iwctl station list | rg ' connected' | awk '{ print $2 }')"
-if [ -n "$station" ]; then
-  station_info="$(iwctl station "$station" show)"
-  ssid="$(echo "${station_info#*Connected network}" | head -n 1 | xargs)"
-fi
-ssid="${ssid:-n/a}"
+exists() {
+  command -v "$1" >/dev/null 2>&1
+}
 
-echo "$ssid | "
+if exists nmcli; then
+  nmcli -t -f DEVICE,TYPE,NAME connection show --active | {
+    IFS=":" read -r device type_ name
+    case "$type_" in
+    *ethernet)
+      ssid="$device"
+      ;;
+    *)
+      ssid="$device:$name"
+      ;;
+    esac
+    echo "$ssid | "
+  }
+elif exists iwctl; then
+  station="$(iwctl station list | rg ' connected' | awk '{ print $2 }')"
+  if [ -n "$station" ]; then
+    station_info="$(iwctl station "$station" show)"
+    ssid="$(echo "${station_info#*Connected network}" | head -n 1 | xargs)"
+  fi
+  ssid="${ssid:-n/a}"
+  echo "$ssid | "
+fi
+
+unset ssid device name
